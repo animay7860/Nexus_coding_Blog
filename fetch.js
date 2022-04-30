1f23a570e32be04010da93276684ea4782f3e739
animay7860 <99870091+animay7860@users.noreply.github.com>
2022-04-30 15:05:26 +0530

    addin

--- fetch.js
+++ fetch.js
@@ -1,3 +1,4 @@
 fetch('https://asutosh-weather-app.herokuapp.com/weather?address=puri').then((response)=>{
-    console.log(response)
+    console.log(response);
+    console.log("my name is animay");
 })
