e58e3d2c88305de4cc5e7fa6c82b886391986afe
Animay Prakash <99870091+animay7860@users.noreply.github.com>
2022-05-14 13:54:18 +0530

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,5 +1,5 @@
 
-fetch('https://asutosh-weather-app.heroku.com/weather?address=puri').then((response) => {
+fetch('https://asutosh-weather-app.herokuapp.com/weather?address=puri').then((response) => {
     response.json().then((data) => {
         if (data.error) {
             console.log(data.error)
